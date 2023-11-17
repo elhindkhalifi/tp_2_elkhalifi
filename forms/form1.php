64f06cf9591b5adadef392fb0999036100f63a6f
elhindkhalifi <elhindkhalifi@gmail.com>
2023-11-16 20:04:47 -0500

    form 1 et 2 marchent

---
+++ forms/form1.php
@@ -0,0 +1,16 @@
+<body>
+<center>
+<h2> </h2>
+<!-- <a href="../index.php">Retour Acceuil</a> -->
+</center>
+ 
+<form method="post" action="form2.php">
+        <input type="hidden" name="action" value="addresses">
+        <label for="address">Combien d'adresses avez-vous ?</label>
+        <input type="number" id="address" name="address" required>
+        <br>
+        <input type="submit" value="submit">
+    </form>
+
+</body>
+</html>
